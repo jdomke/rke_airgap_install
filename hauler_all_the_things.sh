0925a78e6c117d6b1181abe1e33cfc3e91b9383f
domke <673751-domke@users.noreply.gitlab.com>
2026-07-17 18:41:42 +0900

    set token for cluster join

--- hauler_all_the_things.sh
+++ hauler_all_the_things.sh
@@ -25,7 +25,10 @@
 #   USE_SUDO			[false]
 #
 # purge and clean the controller:
-#   systemctl disable --now rke2-server.service hauler-fileserver-amd64@8080 hauler-fileserver-arm64@8081 hauler-registry-amd64@5000 hauler-registry-arm64@5001; umount -l $(mount | grep /run/k3s/containerd | cut -d' ' -f3); dnf list --installed | awk '/@hauler/ {print $1}' | xargs -r sudo dnf remove -y; systemctl daemon-reload; rm -rf ../hauler/* /var/lib/rancher/rke2/ /run/k3s/ /usr/local/bin/{hauler,helm} /tmp/{airgap,hauler,helm}*;
+#   systemctl disable --now rke2-server.service rke2-agent.service hauler-fileserver-amd64@8080 hauler-fileserver-arm64@8081 hauler-registry-amd64@5000 hauler-registry-arm64@5001; umount -l $(mount | grep /run/k3s/containerd | cut -d' ' -f3); dnf list --installed | awk '/@hauler/ {print $1}' | xargs -r dnf remove -y; systemctl daemon-reload; rm -rf ../hauler/* /etc/rancher/rke2/ /var/lib/rancher/rke2/ /run/k3s/ /usr/local/bin/{hauler,helm} /tmp/{airgap,hauler,helm}*;
+#
+# purge and clean a worker
+#   systemctl disable --now rke2-agent.service; dnf list --installed | awk '/@hauler/ {print $1}' | xargs -r dnf remove -y; systemctl daemon-reload; rm -rf /etc/rancher/rke2/ /var/lib/rancher/rke2/ /run/k3s/
 
 # -----------
 # this script is designed to bootstrap a POC cluster using Hauler
